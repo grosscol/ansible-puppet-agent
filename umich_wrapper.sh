26dcb9b9310f18d7f3d7ca1b3c1cadb36e85eb5e
Aaron Elkiss <aelkiss@umich.edu>
2017-03-03 09:28:07 -0500

    accept additional args for ansible

--- umich_wrapper.sh
+++ umich_wrapper.sh
@@ -3,11 +3,12 @@
 if [[ -z $1 || $1 = "-h" || $1 = "--help" ]]
 then
   echo "Useage:"
-  echo "  wrapper.sh path/to/inventory/file"
+  echo "  wrapper.sh path/to/inventory/file [... other ansible args]"
   exit 1
 fi
 
 INVENTORY_FILE=$1
+shift
 
 # Set up a temporary home for ansible related files and control paths.
 #  AFS in the umich setup causes significant problems for running as a regular user.
@@ -24,4 +25,4 @@ export ANSIBLE_SSH_CONTROL_PATH_DIR=${ANSIBLE_TMP_HOME}/control-paths
 export HOME=${ANSIBLE_TMP_HOME}
 
 # Pass the inventory file in as the first arguement
-ansible-playbook -i ${INVENTORY_FILE} agent.playbook.yml
+ansible-playbook -i ${INVENTORY_FILE} agent.playbook.yml "$@"
